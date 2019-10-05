dea023a182223b69161f6231ced25fd405057d8f
zakirovadi <51024712+zakirovadi@users.noreply.github.com>
2019-10-05 16:51:27 +0300

    add slider

--- script.js
+++ script.js
@@ -6,18 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
         macbook = document.getElementsByClassName('macbook')[0],
         notebook = document.getElementsByClassName('notebook')[0];
 
+    var background = document.getElementById('background'),
+        containerService = document.getElementById('service');
 
 
 
 
+//-----------------SCROLL PAGE--------------------------------------
+    var toggleScroll = true;
+
+    window.addEventListener('scroll', function() {
+        if(pageYOffset > background.scrollHeight / 3 && toggleScroll === true){
+            moveSliderService();
+            toggleScroll = false;
+        }
+    });
 
 //-----------MOVE ELEMENTS ON BACKGROUND----------------------
     function moveElements(){
-        cactus.classList.add('cactus-move');
-        macbook.classList.add('macbook-move');
-        ipad.classList.add('ipad-move');
-        phone.classList.add('phone-move');
-        notebook.classList.add('notebook-move');
+        cactus.style.cssText = 'top: 0; left: 0;';
+        macbook.style.cssText = 'top: 0; right: 0;';
+        ipad.style.cssText = 'left: 0;';
+        phone.style.cssText = 'top: 80px;';
+        notebook.style.cssText = 'right: 40%; transform: rotate(0);';
     }
 
 
@@ -101,7 +112,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if(showWatch){
             ipadStyle = getComputedStyle(this);
             ipadUnlock.style.height = ipadStyle.height;
-            console.log(ipadStyle.height);
 
             showWatch = false;
             var date = new Date();
@@ -199,6 +209,130 @@ document.addEventListener('DOMContentLoaded', function () {
     function removeError(){
         
     }
+
+
+
+
+
+
+
+
+//-----------------SERVICE-SLIDER----------------------------
+    const ArrSevice = [
+        {
+            image: 'image/service/phone.png',
+            text: [
+                'замена деталей',
+			    'ремонт после попадания жидкости',
+			    'програмный ремонт (прошивка)',
+			    'ремонт материнской платы'
+            ]
+        },
+        {
+            image: 'image/service/iPadPro.png',
+            text: [
+                '! замена деталей',
+			    '! ремонт после попадания жидкости',
+			    '! програмный ремонт (прошивка)',
+            ]
+        }
+    ];
+
+    var image = document.getElementsByClassName('service-image')[0],
+        list = document.getElementsByClassName('service')[0],
+        counterSlider = 0;
+
+    createPaginator();
+
+    function fixCounter() {
+        if(counterSlider == ArrSevice.length){
+            counterSlider = 0;
+        }
+    };
+
+    function createImg(){
+        image.src = ArrSevice[counterSlider].image;
+        image.style.marginRight = '10%';
+        
+        makeActivePag();
+    }
+
+    function createList(){
+
+        for(var k = 0; k < ArrSevice[counterSlider].text.length; k++){
+            var listItem = document.createElement('li');
+            listItem.innerHTML = ArrSevice[counterSlider].text[k];
+
+            list.appendChild(listItem);
+        }        
+    }
+
+    function removeList(){
+        var listItems = list.children;
+        
+        for(var k = listItems.length - 1; k >= 0; k--){
+            list.removeChild(listItems[k]);            
+        }
+    }
+
+    function createPaginator(){
+        var pag = document.getElementsByClassName('paginator-slider')[0];
+
+        for(var k = 0; k < ArrSevice.length; k++){
+            var pagItem = document.createElement('div');
+
+            pag.appendChild(pagItem);
+        }
+    }
+
+    function createOneSlide(){
+        createImg();
+        createList();
+
+        counterSlider++;
+        fixCounter();
+        return counterSlider;
+    }
+
+    function makeActivePag(){
+        var paginator = document.getElementsByClassName('paginator-slider')[0];
+
+        for(var i = 0; i < paginator.children.length; i++){
+            var pagItem = paginator.children[i];
+            pagItem.style.backgroundColor = '#66666694';
+
+            if(counterSlider == i){
+                pagItem.style.backgroundColor = '#666666';
+            }
+        }
+    }
+
+    function changeOneSlide(){
+        image.style.marginRight = '100%';
+        
+
+        image.addEventListener('transitionend', function(){
+            if(image.style.marginRight == '100%'){
+
+                removeList();
+                createOneSlide(); 
+            }
+        });
+    }
+
+    function moveSliderService(){
+        var title = document.getElementsByClassName('service-title')[0];
+        title.style.right = '20%';
+
+        createOneSlide();
+
+        var idIntervalSlider = setInterval(function(){
+            changeOneSlide();
+        }, 7000);
+    }
+
+
+
 
 
 

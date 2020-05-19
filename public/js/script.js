93cbf6c7d75ccace39f682db5c31f3a13e361a29
Diana Zakirova <zakirovadiana.mk@gmail.com>
2020-05-19 16:57:21 +0300

    create menu, positioning gadgets

--- public/js/script.js
+++ public/js/script.js
@@ -31,13 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //-----------MOVE ELEMENTS ON BACKGROUND----------------------
-    const moveElements = () => {
-        ipad.style.cssText = 'left: 0;';
-        cactus.style.cssText = 'top: 0';
-        macbook.style.cssText = 'top: 0; right: 0;';
-        phone.style.cssText = 'right: 0; bottom: 0';
-        menu.style.cssText = 'left: 0';
-    }
+    // const moveElements = () => {
+    //     ipad.style.cssText = 'left: 0;';
+    //     cactus.style.cssText = 'top: 0';
+    //     macbook.style.cssText = 'top: 0; right: 0;';
+    //     phone.style.cssText = 'right: 0; bottom: 0';
+    //     menu.style.cssText = 'left: 0';
+    // }
 
 
 
@@ -46,75 +46,75 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //--------------SHOW TIME ON IPAD------------------------------
 
-    const addZero = num => num < 10 ? '0' + num : num;
+    // const addZero = num => num < 10 ? '0' + num : num;
 
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    //     month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    function changeOpacitySlowly(elem, func){
-        var id = setInterval(func(elem), 100);
+    // function changeOpacitySlowly(elem, func){
+    //     var id = setInterval(func(elem), 100);
             
-        setTimeout(function(){
-            clearInterval(id);
-        }, 1000)
-    }
+    //     setTimeout(function(){
+    //         clearInterval(id);
+    //     }, 1000)
+    // }
 
-    function addOpacity(elem){
-        var num = 0.1;
+    // function addOpacity(elem){
+    //     var num = 0.1;
 
-        return () => {
-            if (elem.style.opacity < 1){
-                num += 0.1;
-                elem.style.opacity = num;
-                return num;
-            }
-        }
+    //     return () => {
+    //         if (elem.style.opacity < 1){
+    //             num += 0.1;
+    //             elem.style.opacity = num;
+    //             return num;
+    //         }
+    //     }
         
-    }
+    // }
 
-    function deleteOpacity(elem){
-        var num = 1;
+    // function deleteOpacity(elem){
+    //     var num = 1;
 
-        return () => {
-            if (elem.style.opacity > 0){
-                num -= 0.1;
-                elem.style.opacity = num;
-                return num;
-            }
-        }
+    //     return () => {
+    //         if (elem.style.opacity > 0){
+    //             num -= 0.1;
+    //             elem.style.opacity = num;
+    //             return num;
+    //         }
+    //     }
         
-    }    
+    // }    
        
 
-    let isShowWatch = true;
+    // let isShowWatch = true;
 
-    ipad.addEventListener('mouseover', () => {
+    // ipad.addEventListener('mouseover', () => {
 
-        if(isShowWatch){
-            isShowWatch = false;
-            const date = new Date();
+    //     if(isShowWatch){
+    //         isShowWatch = false;
+    //         const date = new Date();
 
-            const ipadWatch = document.getElementsByClassName('ipadWatch')[0],
-                ipadDate = document.getElementsByClassName('ipadDate')[0];
+    //         const ipadWatch = document.getElementsByClassName('ipadWatch')[0],
+    //             ipadDate = document.getElementsByClassName('ipadDate')[0];
 
-            ipadWatch.style.opacity = '0.0';
-            ipadDate.style.opacity = '0.0';
+    //         ipadWatch.style.opacity = '0.0';
+    //         ipadDate.style.opacity = '0.0';
             
-            ipadWatch.innerHTML = date.getHours() + ':' + addZero(date.getMinutes());
-            ipadDate.innerHTML = daysOfWeek[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()];
+    //         ipadWatch.innerHTML = date.getHours() + ':' + addZero(date.getMinutes());
+    //         ipadDate.innerHTML = daysOfWeek[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()];
         
             
-            changeOpacitySlowly(ipadWatch, addOpacity);
-            changeOpacitySlowly(ipadDate, addOpacity);
+    //         changeOpacitySlowly(ipadWatch, addOpacity);
+    //         changeOpacitySlowly(ipadDate, addOpacity);
             
-            setTimeout(function(){
-                changeOpacitySlowly(ipadWatch, deleteOpacity);
-                changeOpacitySlowly(ipadDate, deleteOpacity);
-                isShowWatch = true;
-            }, 10000)
+    //         setTimeout(function(){
+    //             changeOpacitySlowly(ipadWatch, deleteOpacity);
+    //             changeOpacitySlowly(ipadDate, deleteOpacity);
+    //             isShowWatch = true;
+    //         }, 10000)
 
-        }
-    });
+    //     }
+    // });
 
 
 
@@ -123,16 +123,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //-----------MENU-BURGER-----------------------------------
-    var btnMenu = document.getElementsByClassName('menuBtn')[0],
-        menuUl = document.getElementsByClassName('menuUl')[0],
-        nav = document.getElementsByTagName('nav')[0];
+    // var btnMenu = document.getElementsByClassName('menuBtn')[0],
+    //     menuUl = document.getElementsByClassName('menuUl')[0],
+    //     nav = document.getElementsByTagName('nav')[0];
 
-    btnMenu.addEventListener('click', function(){
-        btnMenu.classList.toggle('menuBtn-active');
-        menuUl.classList.toggle('menuUl-show');
-        cactus.classList.toggle('move-cactus');
-        nav.classList.toggle('open-menu');
-    })
+    // btnMenu.addEventListener('click', function(){
+    //     btnMenu.classList.toggle('menuBtn-active');
+    //     menuUl.classList.toggle('menuUl-show');
+    //     cactus.classList.toggle('move-cactus');
+    //     nav.classList.toggle('open-menu');
+    // })
 
 
 

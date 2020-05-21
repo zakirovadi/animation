7103975fbdd429a644f44ce1c2380d4423b90ca0
Diana Zakirova <zakirovadiana.mk@gmail.com>
2020-05-21 16:18:55 +0300

    create slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         {
             image: 'image/service/From the Land.jpg',
-            name: 'Another Cactus',
+            name: 'Cactus 2',
             comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         },
         {
